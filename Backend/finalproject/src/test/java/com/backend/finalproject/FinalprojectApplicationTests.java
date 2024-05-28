7867700a688e77d30d2672660c78afc1f0260684
Breno Amaral <contato.brenoamaral.s@gmail.com>
2024-05-28 15:06:58 -0300

    Adicinando e inicializando a pasta backend

---
+++ Backend/finalproject/src/test/java/com/backend/finalproject/FinalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
