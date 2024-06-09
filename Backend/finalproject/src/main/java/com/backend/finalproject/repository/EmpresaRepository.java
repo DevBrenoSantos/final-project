12b4e393ce6afc7fed0d3645c91b2bba9400f8ea
Breno Amaral <contato.brenoamaral.s@gmail.com>
2024-06-09 19:10:45 -0300

    test

--- Backend/finalproject/src/main/java/com/backend/finalproject/repository/EmpresaRepository.java
+++ Backend/finalproject/src/main/java/com/backend/finalproject/repository/EmpresaRepository.java
@@ -1,8 +1,12 @@
 package com.backend.finalproject.repository;
 
 import com.backend.finalproject.model.Empresa;
+
+import jakarta.persistence.Table;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Table(name = "empresa")
 public interface EmpresaRepository extends JpaRepository<Empresa, Long>{
 	
 }
--- Backend/finalproject/src/main/java/com/backend/finalproject/repository/ProjetoRepository.java
+++ Backend/finalproject/src/main/java/com/backend/finalproject/repository/ProjetoRepository.java
@@ -1,8 +1,12 @@
 package com.backend.finalproject.repository;
 
 import com.backend.finalproject.model.Projeto;
+
+import jakarta.persistence.Table;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Table(name = "projeto")
 public interface ProjetoRepository extends JpaRepository<Projeto, Long>{
 	
 }
