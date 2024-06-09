2ff22d2758a2aa4c33e289fdcc554ff91c072781
Pedro Santayana <me@pedrosantayana.dev>
2024-06-09 20:58:15 -0300

    rota de login

--- new_backend/index.ts
+++ new_backend/index.ts
@@ -1,19 +1,33 @@
-
-
 import express from "express";
+import sqlite3 from "sqlite3";
+
 const app = express();
 const port = 3000;
-
-//sqlite3
-import sqlite3 from "sqlite3";
 const db = new sqlite3.Database("database.db");
-
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
   db.all("SELECT * FROM users", (err, rows) => {
     console.log(rows);
   }
+  );
+});
+
+// reuisição post para login de email e senha
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  db.get(
+    "SELECT * FROM users WHERE email = ? AND password = ?",
+    [email, password],
+    (err, row) => {
+      if (row) {
+        res.send("Login success");
+      } else {
+        res.send("Login failed");
+      }
+    }
   );
 });
 
